0aa61adb8d90391c10d7dd3d862494b79996e091
Anaboliks <73388561+GrigorUN@users.noreply.github.com>
2023-09-05 10:41:27 +0000

    sdada

---
+++ Практика 1/Example.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.println("Введите элементы массива:");
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / size;
+
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое элементов массива: " + average);
+
+        scanner.close();
+
+    }
+}
